886b267e5b35617573c82342bfab5604f7706622
OlegDev <olegdev@OlegDevs-Mac.local>
2017-11-03 08:07:32 -0700

    Added table delegate

--- devslopesShop/Category.swift
+++ devslopesShop/Category.swift
@@ -7,3 +7,18 @@
 //
 
 import Foundation
+
+struct Category {
+    
+    private(set) var title: String
+    private(set) var imageName: String
+    
+    init(title: String , imageName : String) {
+        
+        self.title = title
+        self.imageName = imageName
+    }
+    
+    
+    
+}
--- devslopesShop/devslopesShop/Controller/CategoriesVC.swift
+++ devslopesShop/devslopesShop/Controller/CategoriesVC.swift
@@ -8,7 +8,9 @@
 
 import UIKit
 
-class CategoriesVC: UIViewController {
+class CategoriesVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    
     
     @IBOutlet weak var categoryTable : UITableView!
 
@@ -17,11 +19,15 @@ class CategoriesVC: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    
+        return DataService.instance.getCategories().count
+    
     }
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
 
 }
 
